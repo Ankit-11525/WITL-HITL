d856a791d9dd7bbb21deeeb760341eb8c081e9c2
ankit-11525 <ankitpathak11525@gmail.com>
2024-07-15 09:22:03 +0530

    root directory upadted2

--- witl-hitl/next.config.mjs
+++ witl-hitl/next.config.mjs
@@ -1,53 +1,53 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: true,
-//     images: {
-//         remotePatterns: [
-//             {
-//               hostname: '*.googleusercontent.com',
-//             },
-//           ],
-//           domains: ['res.cloudinary.com'],
-//       },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+              hostname: '*.googleusercontent.com',
+            },
+          ],
+          domains: ['res.cloudinary.com'],
+      },
       
-// };
+};
 
-// export default nextConfig;
+export default nextConfig;
 
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-//   experimental: {
-//     appDir: true,
-//   },
-//   webpack: (config) => {
-//     config.resolve.modules.push(path.resolve('./src'));
-//     return config;
-//   },
-// };
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve('./src'));
+    return config;
+  },
+};
 
 
 // next.config.mjs
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-      remotePatterns: [
-          {
-            hostname: '*.googleusercontent.com',
-          },
-      ],
-      domains: ['res.cloudinary.com'],
-  },
-  experimental: {
-      appDir: true,
-  },
-  webpack: (config) => {
-      const path = require('path');
-      config.resolve.modules.push(path.resolve('./src'));
-      return config;
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//       remotePatterns: [
+//           {
+//             hostname: '*.googleusercontent.com',
+//           },
+//       ],
+//       domains: ['res.cloudinary.com'],
+//   },
+//   experimental: {
+//       appDir: true,
+//   },
+//   webpack: (config) => {
+//       const path = require('path');
+//       config.resolve.modules.push(path.resolve('./src'));
+//       return config;
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
