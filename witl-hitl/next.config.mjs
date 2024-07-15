8609f3d8384e0d5e7c85aca0f71b0065f122e3e3
ankit-11525 <ankitpathak11525@gmail.com>
2024-07-15 09:19:41 +0530

    root directory upadted

--- witl-hitl/next.config.mjs
+++ witl-hitl/next.config.mjs
@@ -1,15 +1,53 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     images: {
+//         remotePatterns: [
+//             {
+//               hostname: '*.googleusercontent.com',
+//             },
+//           ],
+//           domains: ['res.cloudinary.com'],
+//       },
+      
+// };
+
+// export default nextConfig;
+
+// const path = require('path');
+
+// module.exports = {
+//   experimental: {
+//     appDir: true,
+//   },
+//   webpack: (config) => {
+//     config.resolve.modules.push(path.resolve('./src'));
+//     return config;
+//   },
+// };
+
+
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-              hostname: '*.googleusercontent.com',
-            },
-          ],
-          domains: ['res.cloudinary.com'],
-      },
-      
+  reactStrictMode: true,
+  images: {
+      remotePatterns: [
+          {
+            hostname: '*.googleusercontent.com',
+          },
+      ],
+      domains: ['res.cloudinary.com'],
+  },
+  experimental: {
+      appDir: true,
+  },
+  webpack: (config) => {
+      const path = require('path');
+      config.resolve.modules.push(path.resolve('./src'));
+      return config;
+  },
 };
 
 export default nextConfig;
